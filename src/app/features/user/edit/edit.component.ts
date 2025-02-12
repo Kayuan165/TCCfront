508a69d8d8161924f3342f1be0bf7ea2435369f0
Kayuan165 <kayuandiasoli@gmail.com>
2025-02-12 17:07:12 -0300

    refactor: Refatora o componente de edit para melhor performance

--- src/app/features/user/edit/edit.component.ts
+++ src/app/features/user/edit/edit.component.ts
@@ -2,19 +2,25 @@ import { CommonModule } from '@angular/common';
 import {
   Component,
   EventEmitter,
-  inject,
   Input,
   Output,
+  Signal,
   signal,
 } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {
+  FormBuilder,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { User } from '../../../shared/Interfaces/user.interface';
 import { UserService } from '../../../shared/services/user.service';
 import { ToastService } from '../../../shared/components/toast/toast.service';
-import { ToastComponent } from '../../../shared/components/toast/toast.component';
 
 @Component({
   selector: 'app-edit',
+  standalone: true,
   imports: [CommonModule, ReactiveFormsModule, FormsModule],
   templateUrl: './edit.component.html',
   styleUrl: './edit.component.scss',
@@ -23,41 +29,91 @@ export class EditComponent {
   @Input() visitor!: User;
   @Output() visitorUpdated = new EventEmitter<User>();
   @Output() closeModal = new EventEmitter<void>();
-  formVisible = true;
+  form: FormGroup;
   selectedFile = signal<File | null>(null);
+  isSubmitting = signal(false);
 
   constructor(
+    private fb: FormBuilder,
     private userService: UserService,
     private toastService: ToastService
-  ) {}
+  ) {
+    this.form = this.fb.group({
+      name: ['', [Validators.required, Validators.minLength(3)]],
+      rg: ['', [Validators.required, Validators.pattern(/^d{7,14}$/)]],
+      email: ['', [Validators.required, Validators.email]],
+      photo: [null],
+    });
+  }
+
+  ngOnInit() {
+    if (this.visitor) {
+      this.form.patchValue(this.visitor);
+    }
+  }
+
+  get formControls() {
+    return this.form.controls as { [key: string]: any };
+  }
 
   onFileSelected(event: Event) {
     const input = event.target as HTMLInputElement;
 
     if (input.files && input.files.length > 0) {
-      this.selectedFile.set(input.files[0]);
+      const file = input.files[0];
+
+      if (!file.type.startsWith('image/')) {
+        this.toastService.showError(
+          'Por favor, selecione um arquivo de imagem válido!'
+        );
+        return;
+      }
+      if (file.size > 2 * 1024 * 1024) {
+        this.toastService.showError('O tamanho da imagem não pode exceder 2MB');
+        return;
+      }
+
+      this.selectedFile.set(file);
+      this.form.patchValue({ photo: file });
     }
   }
 
   updateVisitor() {
-    if (!this.visitor) return;
-
-    const formData = new FormData();
-    formData.append('name', this.visitor.name);
-    formData.append('rg', this.visitor.rg);
-    formData.append('email', this.visitor.email);
-
-    if (this.selectedFile()) {
-      formData.append('photo', this.selectedFile() as Blob);
+    if (this.form.invalid) {
+      this.toastService.showError('Por favor, corrija os erros do formulário.');
+      return;
     }
 
+    const formData = new FormData();
+
+    if (this.visitor.name) {
+      formData.append('name', this.visitor.name);
+    }
+    if (this.visitor.rg) {
+      formData.append('rg', this.visitor.rg);
+    }
+    if (this.visitor.email) {
+      formData.append('email', this.visitor.email);
+    }
+    const selectedFile = this.selectedFile();
+    if (selectedFile) {
+      formData.append('photo', selectedFile, selectedFile.name);
+    }
+
+    this.isSubmitting.set(true);
     this.userService.update(this.visitor.id, formData).subscribe({
       next: (updateVisitor) => {
+        this.toastService.showSucess('Visitante atualizado com sucesso!');
         this.visitorUpdated.emit(updateVisitor);
         this.closeModal.emit();
+        this.isSubmitting.set(false);
       },
       error: (error) => {
-        console.error('Erro ao cadastrar o visitante:', error);
+        console.error('Erro ao atualizar visitante', error);
+        this.toastService.showError(
+          'Erro ao atualizar o visitante, tente novamente.'
+        );
+        this.isSubmitting.set(false);
       },
     });
   }
