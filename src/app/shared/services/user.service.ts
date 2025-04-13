171f3f4e299a4ab072ad83e64d7fdabdd3f84a62
Kayuan165 <kayuandiasoli@gmail.com>
2025-04-13 14:26:35 -0300

    feat: Adiciona novas validações e novas rotas no service

--- src/app/shared/services/user.service.ts
+++ src/app/shared/services/user.service.ts
@@ -1,7 +1,16 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import {
+  catchError,
+  interval,
+  map,
+  Observable,
+  switchMap,
+  takeWhile,
+} from 'rxjs';
 import { User } from '../Interfaces/user.interface';
+import { error } from 'console';
+import { response } from 'express';
 
 @Injectable({
   providedIn: 'root',
@@ -11,27 +20,104 @@ export class UserService {
 
   constructor(private http: HttpClient) {}
 
-  register(data: FormData): Observable<any> {
-    return this.http.post(`${this.apiUrl}/create`, data);
+  //inicia treinamento
+  startFaceTraining(
+    rg: string
+  ): Observable<{ trainingId: number; status: string }> {
+    return this.http
+      .post<{ trainingId: number; status: string }>(
+        `${this.apiUrl}/init-training`,
+        { rg }
+      )
+      .pipe(
+        catchError((error) => {
+          throw this.handleError(
+            error,
+            'Erro ao iniciar o treinamento facial!'
+          );
+        })
+      );
+  }
+
+  checkTrainingStatus(
+    rg: string
+  ): Observable<{ status: string; ready: boolean }> {
+    return this.http.get<{ status: string; ready: boolean }>(
+      `${this.apiUrl}/training-status/${rg}`
+    );
+  }
+
+  //pooling
+  pollTrainingStatus(rg: string, intervalMs = 2000): Observable<boolean> {
+    return interval(intervalMs).pipe(
+      switchMap(() => this.checkTrainingStatus(rg)),
+      map((response) => response.ready),
+      takeWhile((ready) => !ready, true), // continua até ready=true
+      catchError((error) => {
+        throw this.handleError(
+          error,
+          'Erro ao verificar status do treinamento'
+        );
+      })
+    );
+  }
+
+  //registra
+  completeRegistration(userData: User): Observable<User> {
+    return this.http.post<User>(`${this.apiUrl}/create`, userData).pipe(
+      catchError((error) => {
+        throw this.handleError(error, 'Erro ao registrar usuário!');
+      })
+    );
   }
 
   getAllResident(): Observable<User[]> {
-    return this.http.get<User[]>(`${this.apiUrl}/resident`);
+    return this.http.get<User[]>(`${this.apiUrl}/resident`).pipe(
+      catchError((error) => {
+        throw this.handleError(error, 'Erro ao buscar moradores');
+      })
+    );
   }
 
   getAllVisitors(): Observable<User[]> {
-    return this.http.get<User[]>(`${this.apiUrl}/visitors`);
+    return this.http.get<User[]>(`${this.apiUrl}/visitors`).pipe(
+      catchError((error) => {
+        throw this.handleError(error, 'Erro ao buscar visitantes');
+      })
+    );
   }
 
   get(id: number): Observable<User> {
-    return this.http.get<User>(`${this.apiUrl}/${id}`);
+    return this.http.get<User>(`${this.apiUrl}/${id}`).pipe(
+      catchError((error) => {
+        throw this.handleError(error, 'Erro ao buscar usuário');
+      })
+    );
   }
 
   update(id: number, data: FormData): Observable<any> {
-    return this.http.patch(`${this.apiUrl}/update/${id}`, data);
+    return this.http.patch(`${this.apiUrl}/update/${id}`, data).pipe(
+      catchError((error) => {
+        throw this.handleError(error, 'Erro ao atualizar usuário');
+      })
+    );
   }
 
   delete(id: number): Observable<any> {
-    return this.http.delete(`${this.apiUrl}/delete/${id}`);
+    return this.http.delete(`${this.apiUrl}/delete/${id}`).pipe(
+      catchError((error) => {
+        throw this.handleError(error, 'Erro ao remover usuário');
+      })
+    );
+  }
+
+  private handleError(error: any, defaultMsg: string): Error {
+    console.error('Erro no UserService:', error);
+
+    if (error.error?.message) {
+      throw new Error(error.error.message);
+    }
+
+    throw new Error(defaultMsg);
   }
 }
